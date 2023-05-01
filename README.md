b2f315a0be1df495eeb90f5c11ff895a5851aaab
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2023-05-01 17:56:47 -0300

    adicionar js e readme
    Adicionar js e readme

---
+++ README.md
@@ -0,0 +1,17 @@
+#	Este é um desafio de Código em Javascript para calcular o valor gasto em uma viagem de carro :car:
+
+* Declarei 3 variáveis 
+
+* O preço do combustível foi definido como uma constante
+
+* O km por litro fora definido como uma variável, visto que o código busca ir um pouco além do proposto.
+
+* A distância percorrida fora definida como variável, justamente para experimentar, um pouco mais.
+
+  
+
+  :call_me_hand:   Espero que além de me desenvolver, possa ajudar a contribuir para essa comunidade que me surpreende cada dia mais.
+
+  :tada:   Gostaria de agradecer a todos que se dispõe a ensinar. Obrigado!
+
+​        by: Esdra Felipe :man_dancing:
---
+++ aula1.js
@@ -0,0 +1,37 @@
+/*
+Este é um desafio de projeto para declarar o valor gasto em uma viagem. Existem três variáveis a serem consideradas: distância percorrida, consumo de combustível do carro e preço do combustível.
+o preço do combustível será declarado como uma constante, pois não irá mudar durante a execução do programa. a distância percorrida e o consumo de combustível serão declarados como variáveis, pois poderão mudar durante a execução do programa.
+*/
+
+
+
+const preço_combustivel = 5.39; // R$ por litro
+
+let distancia_km = 540; // km
+
+let gasto_medio_combustivel = 7.5;   // km/l 
+
+let valor_gasto = (distancia_km / gasto_medio_combustivel) * preço_combustivel;
+
+console.log("O valor gasto para percorrer 540km é: " + valor_gasto);
+
+
+
+
+/* Por exemplo: O gps cria um desvio e você percorre 100km a mais, ou o posto de gasolina está fechado e você precisa andar mais 20km para encontrar outro posto.
+*/
+
+
+
+
+distancia_km = distancia_km + 100;
+let valor_gasto_2 = (distancia_km / gasto_medio_combustivel) * preço_combustivel;
+console.log("O valor gasto para percorrer 640km é: " + valor_gasto_2);
+
+/* Suponhamos que o carro tenha um consumo de combustível maior que o esperado, por exemplo, 5km/l.*/
+
+gasto_medio_combustivel = 5;
+let valor_gasto_3 = (distancia_km / gasto_medio_combustivel) * preço_combustivel;
+console.log("O valor gasto para percorrer 640km é: " + valor_gasto_3);
+
+/*Um exemplo simples de como utilzar a linguagem javascript para calcular o valor gasto em uma viagem.*/
